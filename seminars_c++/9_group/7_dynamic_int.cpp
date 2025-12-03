00a8edf2e0c3bd513a2e6ad901f56d72acdbfec3
Egor0212 <egorkozel0156@gmail.com>
2025-12-03 11:21:53 +0300

    add dynamic int problem

---
+++ seminars_c++/9_group/7_dynamic_int.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+
+class DynamicInt {
+    int *p;
+public:
+    DynamicInt(int i);
+    ~DynamicInt() { 
+        delete p;
+        std::cout << "освобождение памяти\n";
+    }
+
+    int get () { return *p; }
+};
+
+DynamicInt::DynamicInt(int i) {
+    p = new int(i);
+}
+
+// Возвращает отрицательное значение *ob.p
+int GetNegative(DynamicInt ob) {
+    return -ob.get();
+}
+
+int main() {
+    DynamicInt obj1(-10);
+    std::cout << obj1.get() << "\n";
+    std::cout << GetNegative(obj1) << "\n";
+
+    DynamicInt obj2(20);
+    std::cout << obj2.get() << "\n";
+    std::cout << GetNegative(obj2) << "\n";
+    
+    std::cout << obj1.get() << "\n";
+    std::cout << GetNegative(obj1) << "\n";
+    
+    return 0 ;
+}
